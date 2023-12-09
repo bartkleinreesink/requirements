5f5db0104d456428e3848cdf928b1aec88063623
Bart Klein Reesink <bartkleinreesink@gmail.com>
2023-12-09 13:56:37 +0100

    Add PHPExtension

---
+++ src/Requirement/PHPExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Fabrikage\Requirements\Requirement;
+
+class PHPExtension extends AbstractRequirement
+{
+    private string $extension;
+
+    public function __construct(string $extension)
+    {
+        $this->extension = $extension;
+
+        parent::__construct();
+    }
+
+    public function isMet(): bool
+    {
+        return extension_loaded($this->extension);
+    }
+
+    public function getErrorMessage(): string
+    {
+        return sprintf('PHP extension %s is not loaded.', $this->extension);
+    }
+}
